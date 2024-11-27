4785b361b8b4bbe909fc89a3745e337fc65ef22a
HaleyMason <153136301+HaleyMason@users.noreply.github.com>
2024-11-26 19:36:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 "use strict";
 
+//Gets the image element by its Id
 const pageIcon = document.getElementById('pageIcon')
 
+//Add an event listener to the image to listen for a click event 
 pageIcon.addEventListener('click', function() {
+    
+    //Check if the image source matches the specific png file
     if (pageIcon.src.includes('LetsEatLogo.png')) {
+
+        //Redirect to the home page 
         window.location.href = '/';
     }
 });

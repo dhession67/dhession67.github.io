1019c3fb0a763154cb0172a36cce557ce768583d
HaleyMason <153136301+HaleyMason@users.noreply.github.com>
2024-12-04 19:28:35 -0600

    Update index.js
    Added 3 hover images for the breakfast, lunch, and dinner page

--- index.js
+++ index.js
@@ -13,3 +13,38 @@ pageIcon.addEventListener('click', function() {
         window.location.href = '/';
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+
+const image1 = document.getElementById('LemonPoppyseedPancakes');
+const image2 = document.getElementById('PimentoCheeseBLT');
+const image3 = document.getElementById('RedWineBraisedBeef');
+
+if (image1) {
+  image1.addEventListener('mouseenter', () => {
+      image1.src = 'Lemon-Poppy-Seed-Pancake2.jpeg'; 
+  });
+  image1.addEventListener('mouseleave', () => {
+      image1.src = 'Lemon-PoppySeed-Pancake-Skinny.jpeg'; 
+  });
+}
+
+if (image2) {
+  image2.addEventListener('mouseenter', () => {
+      image2.src = 'pimento-cheese-blt-sandwich2.jpeg'; 
+  });
+  image2.addEventListener('mouseleave', () => {
+      image2.src = 'pimento-cheese-blt-sandwich.jpeg'; 
+  });
+}
+
+if (image3) {
+  image3.addEventListener('mouseenter', () => {
+      image3.src = 'Red-Wine-Braised-Beef2.jpeg'; 
+  });
+  image3.addEventListener('mouseleave', () => {
+      image3.src = 'Red-Wine-Braised-Beef-Skinny.jpeg'; 
+  });
+}
+
+});
